e4534b58138baf07810c2c1e6a781c02cedaf461
Aidan Boyle <aidan@boylecreations.com>
2015-08-09 18:17:14 -0400

    updated main script

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,17 +8,34 @@
  * Controller of the SandboxApp
  */
 angular.module('SandboxApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', ['$scope', '$location', '$log', function ($scope, $location, $log) {
+   
+
+    //$log.info($location.path());
+    
 
     $scope.alertClick = function() {
 
     	alert("clicked!");
 
     };
+
+    window.addEventListener('hashchange', function() {
+
+      if ( window.location.hash === '#/bookmark/1') {
+          console.log('Hash Changed!: ' + window.location.hash);
+      }
+
+      if ( window.location.hash === '#/bookmark/2') {
+        console.log('Hash Changed!: ' + window.location.hash);
+      }
+
+      if ( window.location.hash === '#/bookmark/3') {
+        console.log('Hash Changed!: ' + window.location.hash);
+      }
+
+      console.log('Hash Changed!: ' + window.location.hash);
+
+    });
     
-  });
+  }]);
